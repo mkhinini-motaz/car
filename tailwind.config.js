6afcfee3a6176707afa3088557c214049dfe99a2
Moatez Mkhinini <moatezmkhinini@think-it.io>
2024-07-18 18:45:05 +0100

    native wind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/App.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/i18n/index.ts",
+    "./src/screens/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       primary: '#88D66C',
