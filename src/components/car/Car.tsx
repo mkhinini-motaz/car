b081f151c3fe2e48694f8857cfdcc8bf4fb7fb46
Moatez Mkhinini <moatezmkhinini@think-it.io>
2024-07-27 21:46:41 +0100

    minimal output on car

--- src/components/car/Car.tsx
+++ src/components/car/Car.tsx
@@ -4,7 +4,7 @@ import Section from "../../components/common/Section";
 import { formatDate, formatDateTime, timeDiff } from "../../support/utils";
 import TranslatableText from "../../components/common/TranslatableText";
 import EvilIconsIcon from "react-native-vector-icons/EvilIcons";
-import MaterialCommunityIconsIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import MaterialCommunityIcon from "react-native-vector-icons/MaterialCommunityIcons";
 import MaterialIconsIcon from "react-native-vector-icons/MaterialIcons";
 import IoniconsIcon from "react-native-vector-icons/Ionicons";
 import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
@@ -14,48 +14,69 @@ import MoneyFormatter from '../common/MoneyFormatter';
 
 export interface CarProps {
   data: CarClass
+  showDetails: boolean
 }
 
-export default function Car({ data }: CarProps): JSX.Element {
+export default function Car({ data, showDetails = false }: CarProps): JSX.Element {
   return (<Section>
-    <LangAwareView className="gap-2 items-center">
+    <LangAwareView className="gap-x-2 items-center">
       <IoniconsIcon name="car-sport-outline" size={30} />
-      <Text className={'text-lg'}>{data.manufacturer + ' - ' + data.model_name + ' - ' + data.model_year}</Text>
+      <Text className={'text-lg'}>{
+        data.manufacturer + ' - ' +
+        data.model_name + ' - ' +
+        data.model_year
+      }</Text>
     </LangAwareView>
 
-    <LangAwareView className="gap-2 items-center">
-      <MaterialIconsIcon name="numbers" size={30} />
-      <Text className={'text-lg'}>{data.licence_plate}</Text>
+    <LangAwareView className="justify-between items-center">
+      <LangAwareView>
+        <MaterialIconsIcon name="numbers" size={30} />
+        <Text className={'text-lg'}>{data.licence_plate}</Text>
+      </LangAwareView>
 
-      <MaterialCommunityIconsIcon name="horse" size={30} />
-      <Text className={'text-lg'}>{data.fiscal_horse_power}</Text>
+      {showDetails ? <LangAwareView className="gap-x-2 items-center justify-between">
+          <MaterialCommunityIcon name="horse" size={30} />
+          <Text className={'text-lg'}>{data.fiscal_horse_power}</Text>
 
-      <MaterialCommunityIconsIcon name="cylinder" size={30} />
-      <Text className={'text-lg'}>{data.number_of_cylinders}</Text>
+          <MaterialCommunityIcon name="cylinder" size={30} />
+          <Text className={'text-lg'}>{data.number_of_cylinders}</Text>
+        </LangAwareView> :
+        <LangAwareView className="items-center">
+          <FontAwesome5Icon name="money-bill-wave" size={30} />
+          <MoneyFormatter data={data.daily_price} className='text-lg' />
+        </LangAwareView>
+      }
     </LangAwareView>
 
-    <LangAwareView className="gap-2 items-center">
-      <MaterialIconsIcon name="local-gas-station" size={30} />
-      <TranslatableText data={'car:fuel_type:' + data.fuel_type} className='text-lg' />
-      <FontAwesomeIcon name="gears" size={30} />
-      <TranslatableText data={'car:transmission_type:' + data.transmission_type} className='text-lg' />
-    </LangAwareView>
+    {showDetails && <LangAwareView className="gap-x-2 items-center justify-between">
+      <LangAwareView className='gap-x-2'>
+        <MaterialIconsIcon name="local-gas-station" size={30} />
+        <TranslatableText data={'car:fuel_type:' + data.fuel_type} className='text-lg' />
+      </LangAwareView>
 
-    <LangAwareView className="gap-2 items-center">
+      <LangAwareView className='gap-x-2'>
+        <FontAwesomeIcon name="gears" size={30} />
+       <TranslatableText data={'car:transmission_type:' + data.transmission_type} className='text-lg' />
+      </LangAwareView>
+    </LangAwareView>}
+
+    {showDetails && <LangAwareView className="gap-x-2 items-center">
       <FontAwesome5Icon name="money-bill-wave" size={30} />
       <MoneyFormatter data={data.daily_price} className='text-lg' />
-    </LangAwareView>
+    </LangAwareView>}
 
-    <LangAwareView className="gap-2 items-center">
-      <TranslatableText data={'car:circulation_tax_ends_at'} params={{ date: formatDate(data.circulation_tax_ends_at) }} className='text-lg' />
-    </LangAwareView>
+    {showDetails && <>
+      <LangAwareView className="gap-x-2 items-center">
+        <TranslatableText data={'car:circulation_tax_ends_at'} params={{ date: formatDate(data.circulation_tax_ends_at) }} className='text-lg' />
+      </LangAwareView>
 
-    <LangAwareView className="gap-2 items-center">
-      <TranslatableText data={'car:insurance_ends_at'} params={{ date: formatDate(data.insurance_ends_at) }} className='text-lg' />
-    </LangAwareView>
+      <LangAwareView className="gap-x-2 items-center">
+        <TranslatableText data={'car:insurance_ends_at'} params={{ date: formatDate(data.insurance_ends_at) }} className='text-lg' />
+      </LangAwareView>
 
-    <LangAwareView className="gap-2 items-center">
-      <TranslatableText data={'car:technical_control_ends_at'} params={{ date: formatDate(data.technical_control_ends_at) }} className='text-lg' />
-    </LangAwareView>
+      <LangAwareView className="gap-x-2 items-center">
+        <TranslatableText data={'car:technical_control_ends_at'} params={{ date: formatDate(data.technical_control_ends_at) }} className='text-lg' />
+      </LangAwareView>
+    </>}
   </Section>);
 }
