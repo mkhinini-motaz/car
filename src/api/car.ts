595c5a7a7261adc2907864d8e475cddbeee66b52
Moatez Mkhinini <moatezmkhinini@think-it.io>
2024-07-28 04:45:38 +0100

    fix car index error

--- src/api/car.ts
+++ src/api/car.ts
@@ -4,9 +4,11 @@ import { getUserToken } from "../support/auth";
 import { API_URL } from '../constants';
 
 export function index({ queryKey }): Promise<Car> {
-  const [_key, { search }] = queryKey;
-  console.log({_key, queryKey})
-  const params = { search };
+  let params = {};
+  try {
+    const [_key, { search }] = queryKey;
+    params = { search };
+  } catch (error) {}
 
   return getUserToken().then(token => {
       return axios.get(API_URL + 'car', { params, headers: { Authorization: 'Bearer ' + token }}).then(response => {
